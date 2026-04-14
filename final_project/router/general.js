4265472a14e9b5ebefde8f46adf0fbc5ec530e91
YiYang <yangyi040609@gmail.com>
2026-04-14 03:20:46 +0200

    Align tasks 10-13 Axios logic with primary general routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const BASE_URL = `http://127.0.0.1:${process.env.PORT || 5000}`;
 
 
 public_users.post("/register", (req,res) => {
@@ -21,13 +22,12 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({ message: "Unable to register user." });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
+// Internal sync routes for tasks 10-13 Axios wrappers
+public_users.get('/sync/books', function (req, res) {
   return res.send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/sync/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
 
@@ -36,10 +36,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
   }
 
   return res.send(book);
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+});
+
+public_users.get('/sync/author/:author', function (req, res) {
   const author = req.params.author.toLowerCase();
   const booksByAuthor = {};
 
@@ -52,8 +51,7 @@ public_users.get('/author/:author',function (req, res) {
   return res.send(booksByAuthor);
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/sync/title/:title', function (req, res) {
   const title = req.params.title.toLowerCase();
   const booksByTitle = {};
 
@@ -64,6 +62,45 @@ public_users.get('/title/:title',function (req, res) {
   });
 
   return res.send(booksByTitle);
+});
+
+// Task 10: Get all books using async/await + Axios
+public_users.get('/', async function (req, res) {
+  try {
+    const response = await axios.get(`${BASE_URL}/sync/books`);
+    return res.send(response.data);
+  } catch (_) {
+    return res.status(500).json({ message: "Unable to fetch books asynchronously" });
+  }
+});
+
+// Task 11: Get book details by ISBN using Promise callbacks + Axios
+public_users.get('/isbn/:isbn', function (req, res) {
+  axios.get(`${BASE_URL}/sync/isbn/${encodeURIComponent(req.params.isbn)}`)
+    .then((response) => res.send(response.data))
+    .catch((error) => {
+      if (error.response && error.response.status === 404) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+      return res.status(500).json({ message: "Unable to fetch book by ISBN asynchronously" });
+    });
+});
+
+// Task 12: Get books by author using async/await + Axios
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const response = await axios.get(`${BASE_URL}/sync/author/${encodeURIComponent(req.params.author)}`);
+    return res.send(response.data);
+  } catch (_) {
+    return res.status(500).json({ message: "Unable to fetch books by author asynchronously" });
+  }
+});
+
+// Task 13: Get books by title using Promise callbacks + Axios
+public_users.get('/title/:title', function (req, res) {
+  axios.get(`${BASE_URL}/sync/title/${encodeURIComponent(req.params.title)}`)
+    .then((response) => res.send(response.data))
+    .catch(() => res.status(500).json({ message: "Unable to fetch books by title asynchronously" }));
 });
 
 //  Get book review
@@ -80,40 +117,6 @@ public_users.get('/review/:isbn',function (req, res) {
   }
 
   return res.send(book.reviews);
-});
-
-// Task 10: Get all books using async/await + Promise helper
-public_users.get('/async/books', async function (req, res) {
-  try {
-    const response = await axios.get("http://localhost:5000/");
-    return res.send(response.data);
-  } catch (error) {
-    return res.status(500).json({ message: "Unable to fetch books asynchronously" });
-  }
-});
-
-// Task 11: Get book by ISBN using Promise callbacks + Axios
-public_users.get('/async/isbn/:isbn', function (req, res) {
-  axios.get(`http://localhost:5000/isbn/${req.params.isbn}`)
-    .then((response) => res.send(response.data))
-    .catch(() => res.status(404).json({ message: "Book not found" }));
-});
-
-// Task 12: Get books by author using async/await + Axios
-public_users.get('/async/author/:author', async function (req, res) {
-  try {
-    const response = await axios.get(`http://localhost:5000/author/${req.params.author}`);
-    return res.send(response.data);
-  } catch (_) {
-    return res.status(500).json({ message: "Unable to fetch books by author asynchronously" });
-  }
-});
-
-// Task 13: Get books by title using Promise callbacks + Axios
-public_users.get('/async/title/:title', function (req, res) {
-  axios.get(`http://localhost:5000/title/${encodeURIComponent(req.params.title)}`)
-    .then((response) => res.send(response.data))
-    .catch(() => res.status(500).json({ message: "Unable to fetch books by title asynchronously" }));
 });
 
 module.exports.general = public_users;
